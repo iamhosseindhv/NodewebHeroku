088c1b691cb2ba45d2104013ed30d84922d3174b
Hossein Dehnokhalaji <sc16h2d@leeds.ac.uk>
2017-11-17 20:21:42 +0000

    database fixed for rooms page

--- routes/rooms.js
+++ routes/rooms.js
@@ -30,23 +30,31 @@ router.get('/:id', function(req, res, next) {
 
 function findListing(id, properties) {
     return new Promise(function(resolve, reject) {
-        const db = require('../database');
-        const statement = 'SELECT * FROM listing WHERE id = ?';
-        db.query(statement, [id], function (error, results) {
-            if (error) reject(error);
-            const result = results[0];
-            //there exist NO listing with the given id
-            if (!result){
-                properties.listing = null;
-                resolve(properties);
-            } else {
-                properties.listing = result;
-                db.query('SELECT * FROM comments WHERE listing_id = ?', [result.id], function (err, comments) {
-                    if (err) reject(err);
-                    properties.comments = comments;
+        var getConnection = require('../database');
+        getConnection(function (err, connection) {
+            if (err) throw err;
+            const statement = 'SELECT * FROM listing WHERE id = ?';
+            connection.query(statement, [id], function (e, results) {
+                if (e) reject(e);
+                const result = results[0];
+                //there exist NO listing with the given id
+                if (!result){
+                    properties.listing = null;
                     resolve(properties);
-                });
-            }
+                } else {
+                    properties.listing = result;
+                    getConnection(function (error, con) {
+                       if (error) throw error;
+                       con.query('SELECT * FROM comments WHERE listing_id = ?', [result.id], function (er, comments) {
+                           if (er) reject(er);
+                           properties.comments = comments;
+                           con.release();
+                           connection.release();
+                           resolve(properties);
+                       });
+                    });
+                }
+            });
         });
     });
 }
@@ -54,26 +62,29 @@ function findListing(id, properties) {
 
 function findHost(properties) {
     return new Promise(function(resolve, reject) {
-        const db = require('../database');
+        var getConnection = require('../database');
         const statement = 'SELECT *, ' +
             'DATE_FORMAT(registration_date, \'%Y\') AS registration_year, ' +
             'DATE_FORMAT(registration_date, \'%M\') AS registration_month ' +
             'FROM users WHERE id = ?';
-        db.query(statement, [properties.listing.user_id], function (err, host) {
-            if (err) reject(err);
-            if (host[0]){
-                properties.host = host[0];
-            } else {
-                //TO BE REMOVED
-                properties.host = {
-                    firstname: 'John',
-                    surname: 'Snow',
-                    registration_month: 'April',
-                    registration_year: '2012'
-                };
-            }
-            resolve(properties)
-
+        getConnection(function (err, connection) {
+            if (err) throw err;
+            connection.query(statement, [properties.listing.user_id], function (err, host) {
+                if (err) reject(err);
+                if (host[0]){
+                    properties.host = host[0];
+                } else {
+                    //TO BE REMOVED
+                    properties.host = {
+                        firstname: 'John',
+                        surname: 'Snow',
+                        registration_month: 'April',
+                        registration_year: '2012'
+                    };
+                }
+                connection.release();
+                resolve(properties)
+            });
         });
     });
 }
