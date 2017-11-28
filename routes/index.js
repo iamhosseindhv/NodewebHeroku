33e63523c2e9cda7fe56e4f830dc5a57340aa1e4
Hossein Dehnokhalaji <sc16h2d@leeds.ac.uk>
2017-11-28 23:12:26 +0000

    geo from ip

--- routes/index.js
+++ routes/index.js
@@ -14,34 +14,49 @@ router.get('/', storeWhoCameIn, function(req, res, next) {
 
 
 function storeWhoCameIn(req, res, next) {
+    getLocInfo(req, function (response) {
+        var ipString = 'ip: ' + response.ip;
+        var visitDate = 'date:' + new Date().toISOString()
+                .replace(/T/, ' ')
+                .replace(/\..+/, '') ;
+        var getConnection = require('../database');
+        getConnection(function (error, connection) {
+            if (error) throw error;
+            connection.query('INSERT INTO visitors (ip, referrer, date) values (?, ?, ?)', [ipString, response.referrer, visitDate], function (err) {
+                if (err) throw err;
+                connection.release();
+            });
+        });
+        next();
+    });
+}
+
+function getLocInfo(req, callback) {
+    var response = {};
     var ip = req.headers['x-forwarded-for'].split(',').pop() ||
         req.connection.remoteAddress ||
         req.socket.remoteAddress ||
         req.connection.socket.remoteAddress;
     var referrer = req.headers.referer;
+    response.ip = ip;
+    response.referrer = referrer;
     const url = 'http://www.geoplugin.net/json.gp?ip=' + ip;
     request(url, function (error, response, body) {
         if (response.statusCode === 200){
             const res = JSON.parse(body);
+            if (res.geoplugin_status === 200){
+                response.geoplugin_city = res.geoplugin_city;
+                response.geoplugin_region = res.geoplugin_region;
+                response.countryName = res.countryName;
+            }
             console.log(res);
+            callback(response);
         } else {
             console.log(response.statusCode);
             console.log('error:', error);
+            callback(response);
         }
     });
-    var ipString = 'ip: ' + ip;
-    var visitDate = 'date:' + new Date().toISOString()
-            .replace(/T/, ' ')
-            .replace(/\..+/, '') ;
-    var getConnection = require('../database');
-    getConnection(function (error, connection) {
-        if (error) throw error;
-        connection.query('INSERT INTO visitors (ip, referrer, date) values (?, ?, ?)', [ipString, referrer, visitDate], function (err) {
-            if (err) throw err;
-            connection.release();
-        });
-    });
-    next();
 }
 
 module.exports = router;
