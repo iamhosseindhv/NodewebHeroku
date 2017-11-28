b139696b690cee3af1024f2be86d6a7510ca675b
Hossein Dehnokhalaji <sc16h2d@leeds.ac.uk>
2017-11-28 23:33:27 +0000

    geo from ip

--- routes/index.js
+++ routes/index.js
@@ -14,15 +14,19 @@ router.get('/', storeWhoCameIn, function(req, res, next) {
 
 
 function storeWhoCameIn(req, res, next) {
-    getLocInfo(req, function (response) {
-        var ipString = 'ip: ' + response.ip;
-        var visitDate = 'date:' + new Date().toISOString()
-                .replace(/T/, ' ')
-                .replace(/\..+/, '') ;
+    var ip = req.headers['x-forwarded-for'].split(',').pop() ||
+        req.connection.remoteAddress ||
+        req.socket.remoteAddress ||
+        req.connection.socket.remoteAddress;
+    var ipString = 'ip: ' + ip;
+    var referrer = req.headers.referer;
+    var visitDate = 'date:' + new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '');
+    getLocInfo(ip, function (response) {
+        console.log(response);
         var getConnection = require('../database');
         getConnection(function (error, connection) {
             if (error) throw error;
-            connection.query('INSERT INTO visitors (ip, referrer, date) values (?, ?, ?)', [ipString, response.referrer, visitDate], function (err) {
+            connection.query('INSERT INTO visitors (ip, referrer, date) values (?, ?, ?)', [ipString, referrer, visitDate], function (err) {
                 if (err) throw err;
                 connection.release();
             });
@@ -31,31 +35,23 @@ function storeWhoCameIn(req, res, next) {
     });
 }
 
-function getLocInfo(req, callback) {
-    var response = {};
-    var ip = req.headers['x-forwarded-for'].split(',').pop() ||
-        req.connection.remoteAddress ||
-        req.socket.remoteAddress ||
-        req.connection.socket.remoteAddress;
-    var referrer = req.headers.referer;
-    response.ip = ip;
-    response.referrer = referrer;
+function getLocInfo(ip, callback) {
     const url = 'http://www.geoplugin.net/json.gp?ip=' + ip;
     request(url, function (error, response, body) {
         if (response.statusCode === 200){
             const res = JSON.parse(body);
             if (res.geoplugin_status === 200){
+                var response = {};
                 response.geoplugin_city = res.geoplugin_city;
                 response.geoplugin_region = res.geoplugin_region;
                 response.countryName = res.countryName;
+                callback(response);
             }
-            console.log(res);
-            callback(response);
         } else {
             console.log(response.statusCode);
             console.log('error:', error);
-            callback(response);
         }
+        callback(null);
     });
 }
 
