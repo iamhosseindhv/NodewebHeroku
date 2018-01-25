11f7a4955a9615ab8082c708a1078b7cf2ad2c4e
Hossein DHV <iamhosseindhv@Hosseins-MacBook-Pro.local>
2018-01-25 20:07:16 +0000

    markers customised

--- public/javascripts/searchMap/searchMap.js
+++ public/javascripts/searchMap/searchMap.js
@@ -110,20 +110,76 @@ function itemDidSelect() {
     myBundle.doQueryFromSearchedAddress(place);
 }
 
+function CustomMarker(latlng, map, args) {
+    this.latlng = latlng;
+    this.args = args;
+    this.setMap(map);
+}
+
 //creates new markers
 var markers = [];
 function createMarker(lat, lng, id) {
-    const point = new google.maps.LatLng(
-        parseFloat(lat),
-        parseFloat(lng));
-    const marker = new google.maps.Marker({
-        map: map,
-        id: id,
-        position: point,
-        optimized: true,
-        zIndex: 3999
-    });
-    markers.push(marker);
+
+    CustomMarker.prototype = new google.maps.OverlayView();
+    CustomMarker.prototype.draw = function() {
+
+        var self = this;
+        var div = this.div;
+        if (!div) {
+            div = this.div = document.createElement('div');
+            div.className = 'marker';
+            div.style.position = 'absolute';
+            div.style.cursor = 'pointer';
+            div.style.width = '20px';
+            div.style.height = '20px';
+            div.style.background = 'blue';
+            if (typeof(self.args.marker_id) !== 'undefined') {
+                div.dataset.marker_id = self.args.marker_id;
+            }
+            google.maps.event.addDomListener(div, "click", function(event) {
+                google.maps.event.trigger(self, "click");
+            });
+            var panes = this.getPanes();
+            panes.overlayImage.appendChild(div);
+        }
+        var point = this.getProjection().fromLatLngToDivPixel(this.latlng);
+        if (point) {
+            div.style.left = point.x + 'px';
+            div.style.top = point.y + 'px';
+        }
+    };
+
+    CustomMarker.prototype.remove = function() {
+        if (this.div) {
+            this.div.parentNode.removeChild(this.div);
+            this.div = null;
+        }
+    };
+
+    CustomMarker.prototype.getPosition = function() {
+        return this.latlng;
+    };
+
+    // const point = new google.maps.LatLng(
+    //     parseFloat(lat),
+    //     parseFloat(lng));
+    // const marker = new google.maps.Marker({
+    //     map: map,
+    //     id: id,
+    //     position: point,
+    //     optimized: true,
+    //     zIndex: 3999
+    // });
+    var myLatlng = new google.maps.LatLng(parseFloat(lat), parseFloat(lng));
+    var overlay = new CustomMarker(myLatlng, map,
+        {
+            marker_id: id
+        }
+    );
+    // overlay.remove();
+    // console.log(overlay.getPosition());
+    // markers.push(marker);
+    markers.push(overlay);
 }
 
 
